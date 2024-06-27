93fed290ab2694b079538d661e4be097b3bb6c94
Nicherke <nicksoncheruiyot001@gmail.com>
2024-06-27 15:59:56 +0300

    first java commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) {
+        double var1=17;
+        double var2=8;
+        double var3=11;
+        double var4=13;
+        double var5=var1+var2-var4;
+        double var6=var2*var3/var5;
+        String var7="The answers are: ";
+
+        out.println(var7);
+        out.println(var5);
+        out.println(var6);
+    }
+}
